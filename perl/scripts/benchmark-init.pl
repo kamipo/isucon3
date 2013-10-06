b236a985cd8bf4a8ce5e843732c359411e747781
Kazuhiro Osawa <yappo@shibuya.pl>
2013-10-06 16:59:02 +0900

    oops

--- perl/scripts/benchmark-init.pl
+++ perl/scripts/benchmark-init.pl
@@ -10,8 +10,8 @@ my $app = Isucon3::Web->new("$root_dir/..");
 `cat /home/isucon/webapp/config/add-schema.sql | mysql -uisucon isucon`;
 
 # create user cache
-my $users = $self->dbh->select_all('SELECT id, username, password, salt FROM user');
+my $users = $appf->dbh->select_all('SELECT id, username, password, salt FROM user');
 for my $user (@$users) {
-    $self->memd->set($app->userid_key($user->{id}), join("\t", $user->{username}, $user->{password}, $user->{salt}));
-    $self->memd->set($app->username_key($user->{username}), join("\t", $user->{id}, $user->{password}, $user->{salt}));
+    $app->memd->set($app->userid_key($user->{id}), join("\t", $user->{username}, $user->{password}, $user->{salt}));
+    $app->memd->set($app->username_key($user->{username}), join("\t", $user->{id}, $user->{password}, $user->{salt}));
 }
