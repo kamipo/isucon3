80ebddda2977141eb71183360005d02515790200
Kazuhiro Osawa <yappo@shibuya.pl>
2013-10-06 17:00:25 +0900

    fuck

--- perl/scripts/benchmark-init.pl
+++ perl/scripts/benchmark-init.pl
@@ -10,7 +10,7 @@ my $app = Isucon3::Web->new("$root_dir/..");
 `cat /home/isucon/webapp/config/add-schema.sql | mysql -uisucon isucon`;
 
 # create user cache
-my $users = $appf->dbh->select_all('SELECT id, username, password, salt FROM user');
+my $users = $app->dbh->select_all('SELECT id, username, password, salt FROM user');
 for my $user (@$users) {
     $app->memd->set($app->userid_key($user->{id}), join("\t", $user->{username}, $user->{password}, $user->{salt}));
     $app->memd->set($app->username_key($user->{username}), join("\t", $user->{id}, $user->{password}, $user->{salt}));
