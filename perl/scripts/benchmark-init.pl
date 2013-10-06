8241719bc961e1219196b2900e70d14bf17f70f1
Kazuhiro Osawa <yappo@shibuya.pl>
2013-10-06 14:37:32 +0900

    sql file の方に public_memos を初期化するのを入れた

--- perl/scripts/benchmark-init.pl
+++ perl/scripts/benchmark-init.pl
@@ -4,4 +4,3 @@ use warnings;
 use lib '/home/isucon/webapp/perl/local/lib/perl5';
 
 `cat /home/isucon/webapp/config/add-schema.sql | mysql -uisucon isucon`;
-`echo 'INSERT INTO public_memos (memo_id) SELECT id FROM memos ORDER BY id' | mysql -uisucon isucon`;
